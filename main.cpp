d75dba5ab6c9cc6920513d45a0338262b8415931
nicoloroveri <116441124+nicoloroveri@users.noreply.github.com>
2022-11-27 18:05:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-int a=1, b=0, c=0, n=0;
-cin >> n;
+int a=1, b=0, c=0, n=0;cin >> n;
 if(n>0){
    cout << a <<endl;
-}
 while(c<n){
    c=a+b;
    b=a;
